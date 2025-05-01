4c14d5a080b4ae8402047d34f099409b18ca43eb
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-05-01 18:59:21 +0530

    added code

---
+++ src/main/java/com/ashish/QuickDish/Entity/OrderItem.java
@@ -0,0 +1,29 @@
+package com.ashish.QuickDish.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Food food;
+
+    private int quantity;
+
+    private Long totalPrice;
+
+    @OneToMany
+    private List<IngredientsItem> ingredients;
+}
---
+++ src/test/java/com/ashish/QuickDish/QuickDishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.QuickDish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickDishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
