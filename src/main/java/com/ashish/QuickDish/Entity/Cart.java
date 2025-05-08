b6d206be9c1205965f5f9ff4c46ffbc6d7eeb05c
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-05-09 02:13:00 +0530

    changes

--- src/main/java/com/ashish/QuickDish/Entity/Cart.java
+++ src/main/java/com/ashish/QuickDish/Entity/Cart.java
@@ -1,27 +1,20 @@
 package com.ashish.QuickDish.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
-    private  User customer;
+    private User user;
 
-    private Long total;
-
-    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,orphanRemoval = true)
-    private List<CartItem> item = new ArrayList<>();
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL)
+    private List<CartItem> cartItems;
 }
