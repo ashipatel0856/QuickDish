5ff23c9caf9b1dbf737fc2853085586991ec4355
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-05-21 00:46:31 +0530

    changes

--- src/main/java/com/ashish/QuickDish/QuickDishApplication.java
+++ src/main/java/com/ashish/QuickDish/QuickDishApplication.java
@@ -15,8 +15,9 @@ public class QuickDishApplication {
 		System.setProperty("SPRING_MAIL_USERNAME", dotenv.get("SPRING_MAIL_USERNAME"));
 		System.setProperty("SPRING_MAIL_PASSWORD", dotenv.get("SPRING_MAIL_PASSWORD"));
 		System.setProperty("GOOGLE_MAPS_API_KEY", dotenv.get("GOOGLE_MAPS_API_KEY"));
-//		System.setProperty("User_DB", dotenv.get("User_DB"));
-//		System.setProperty("USER_PASSWORD", dotenv.get("USER_PASSWORD"));
+		System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+		System.setProperty("USER_DB", dotenv.get("USER_DB"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(QuickDishApplication.class, args);
 	}
 }
